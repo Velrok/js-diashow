c4058dbdf869432bd3837f6693ef75509da38f68
Waldemar Schwan <waldemar.schwan@adcloud.com>
2012-12-23 21:40:58 +0100

    update: looks fine now

--- cgi-bin/images_list.py
+++ cgi-bin/images_list.py
@@ -1,14 +1,38 @@
 #!/usr/bin/env python
-
 import os
 import json
+import qrcode
 
 print "Content-type: application/json"
 print
 
 image_dir = "images/"
+qr_dir = "qrcodes/"
+webbase = "http://192.168.1.108:8000/"
 
-def prefix_with_image_dir(x):
-	return "{}{}".format(image_dir, x)
+def generate_qr(destination, content):
+	qr = qrcode.QRCode()
+	qr.add_data(content)
+	qr.make()
+	qr.make_image().save(destination)
 
-print json.dumps(map(prefix_with_image_dir, os.listdir("images/")))
+entries = []
+for d in os.listdir("images/"):
+	if not ( d.lower().endswith('png')
+					 or d.lower().endswith('jpg')
+					 or d.lower().endswith('jpeg') ):
+		continue;
+
+	e = {}
+	image_url = "{}{}".format(image_dir, d)
+	e['image'] = image_url
+
+	qr = "{}{}".format(qr_dir, d)
+	if not os.path.exists(qr):
+		# print image_url
+		generate_qr(qr, webbase + image_url)
+
+	e['qrcode'] = "{}{}".format(qr_dir, d)
+	entries.append(e)
+
+print json.dumps(entries)
